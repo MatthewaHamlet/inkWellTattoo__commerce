391e1cebdb75f4b26f026d2c7f2bf8e3d658cea6
Matthew Hamlet <matthewahamlet@gmail.com>
2019-03-27 07:07:49 -0600

    Adding files from local dir.

---
+++ javascripts/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+
+//  Navigation toggle; blur background (#main) on click.
+$('.navbar-toggler-icon').click(function() {
+    console.log('working here!');
+    $('#main').toggleClass('blur');
+});
+
+
+
+
+}); 
